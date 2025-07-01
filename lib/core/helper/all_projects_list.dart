ef8ccc03f0c054b66ae956733f197cb72c210188
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 18:22:25 +0300

    Add visibility_detector dependency and update project list with new projects

---
+++ lib/core/helper/all_projects_list.dart
@@ -0,0 +1,54 @@
+import 'package:portfolio/core/entites/project_entity.dart';
+import 'package:portfolio/core/utils/assets.dart';
+
+const List<ProjectEntity> allProjects = [
+  ProjectEntity(
+    url: 'https://github.com/abd0-kha1ed/path_tracker',
+    imagePath: Assets.imagesPathTracker,
+    type: 'Robot Controller',
+    title: 'Path Tracker',
+    description:
+        'A Flutter-based mobile app that controls a ground-tracking robot. It displays real-time data such as speed, obstacle distance, and robot status, and allows interactive control of movement and path tracking.',
+  ),
+
+  ProjectEntity(
+    url: 'https://github.com/abd0-kha1ed/attendance-system',
+    imagePath: Assets.imagesLms,
+    type: 'Attendance System',
+    title: 'Flutter Attendance',
+    description:
+        'A Flutter-based attendance system that allows teachers to manage student attendance efficiently.',
+  ),
+  ProjectEntity(
+    url: 'https://github.com/abd0-kha1ed/lms',
+    imagePath: Assets.imagesAttendance,
+    type: 'Flutter LMS',
+    title: 'Learning Platform',
+    description:
+        'Flutter LMS built for students and teachers with clean architecture and state management.',
+  ),
+  ProjectEntity(
+    url: 'https://github.com/mahmoudahmed1718/ecommerce',
+    imagePath: Assets.imagesAttendance,
+    type: 'Fruit Hub',
+    title: 'E-Commerce App',
+    description:
+        'Fruit Hub is a sleek and user-friendly Flutter-based eCommerce app for browsing and buying fresh fruits online',
+  ),
+  ProjectEntity(
+    url: 'https://github.com/mahmoudahmed1718/ecommercedash',
+    imagePath: Assets.imagesAttendance,
+    type: 'Fruit Hub Admin Dashboard',
+    title: 'eCommerce Dashboard',
+    description:
+        'An intuitive Flutter-powered eCommerce dashboard for managing products, orders, and customers with ease',
+  ),
+  ProjectEntity(
+    url: 'https://github.com/mahmoudahmed1718',
+    imagePath: Assets.imagesAttendance,
+    type: 'Quotes App',
+    title: 'Quotes App',
+    description:
+        'A beautifully designed Flutter app that delivers daily inspirational and motivational quotes.',
+  ),
+];
