6ee74ff9d9f6fe3b596faea994b00e8cc08ea8f3
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 17:28:16 +0300

    Add asset paths and definitions for image resources

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAttendance
+  /// assets/images/attendance.png
+  static const String imagesAttendance = "assets/images/attendance.png";
+
+  /// Assets for imagesFacebook
+  /// assets/images/facebook.png
+  static const String imagesFacebook = "assets/images/facebook.png";
+
+  /// Assets for imagesInsta
+  /// assets/images/insta.png
+  static const String imagesInsta = "assets/images/insta.png";
+
+  /// Assets for imagesLms
+  /// assets/images/lms.png
+  static const String imagesLms = "assets/images/lms.png";
+
+  /// Assets for imagesMahmoudahmed
+  /// assets/images/mahmoudahmed.jpeg
+  static const String imagesMahmoudahmed = "assets/images/mahmoudahmed.jpeg";
+
+  /// Assets for imagesNew
+  /// assets/images/new.png
+  static const String imagesNew = "assets/images/new.png";
+
+  /// Assets for imagesPathTracker
+  /// assets/images/path_tracker.png
+  static const String imagesPathTracker = "assets/images/path_tracker.png";
+
+  /// Assets for imagesProjectArrow
+  /// assets/images/project_arrow.png
+  static const String imagesProjectArrow = "assets/images/project_arrow.png";
+
+  /// Assets for imagesProjectArrow2
+  /// assets/images/project_arrow_2.png
+  static const String imagesProjectArrow2 = "assets/images/project_arrow_2.png";
+
+  /// Assets for imagesQuotes
+  /// assets/images/quotes.png
+  static const String imagesQuotes = "assets/images/quotes.png";
+
+  /// Assets for imagesTwiter
+  /// assets/images/twiter.png
+  static const String imagesTwiter = "assets/images/twiter.png";
+
+  /// Assets for imagesWhatIDoArrow
+  /// assets/images/what_i_do_arrow.png
+  static const String imagesWhatIDoArrow = "assets/images/what_i_do_arrow.png";
+}
+
