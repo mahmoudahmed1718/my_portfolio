4d831d9e1152a28c51ebff406f617e23af9285a5
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 17:44:35 +0300

    Add ProjectEntity class to define project data structure

---
+++ lib/core/entites/project_entity.dart
@@ -0,0 +1,15 @@
+class ProjectEntity {
+  final String imagePath;
+  final String type;
+  final String title;
+  final String description;
+  final String url;
+
+  const ProjectEntity({
+    required this.imagePath,
+    required this.type,
+    required this.title,
+    required this.description,
+    required this.url,
+  });
+}
