3e506ed612ad1a23a03b894a3727e4b221ed17e8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 17:53:22 +0300

    Add GradientText widget for displaying text with gradient effects

---
+++ lib/features/home/presentation/view/widgets/gradient_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GradientText extends StatelessWidget {
+  final String text;
+  final TextStyle style;
+  final Gradient gradient;
+
+  const GradientText(
+    this.text, {
+    super.key,
+    required this.gradient,
+    required this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (bounds) {
+        return gradient.createShader(
+          Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+        );
+      },
+      child: SelectableText(text, style: style, textAlign: TextAlign.center),
+    );
+  }
+}
