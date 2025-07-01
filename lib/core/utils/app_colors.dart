3dd2d35bcaf200d7db453698e38c5124bbc539a2
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 17:31:50 +0300

    Add AppColors and AppTextStyles classes for consistent color and text styling

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF272727);
+  static const Color backgroundCards = Color(0xFF323443);
+  static const Color primaryText = Color(0xFFCCD6F6);
+  static const Color secondaryText = Color(0xFF8892B0);
+  static const Color normalText = Color(0xFF4D4D4D);
+  static const Color accent = Color(0xFF3BF686);
+  static const Color accentLight = Color(0xFF80FFA3);
+  static const Color accentBlue = Color(0xFF4CA9FF);
+  static const Color blueDark = Color(0xFF4CA9FF);
+  static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+}
---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+
+class AppTextStyles {
+  static const TextStyle greeting = TextStyle(
+    fontSize: 18,
+    color: AppColors.accent,
+    letterSpacing: 1.2,
+  );
+  static const TextStyle bold56 = TextStyle(
+    color: Colors.white,
+    fontSize: 56,
+    fontWeight: FontWeight.w700,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle bold36 = TextStyle(
+    color: Colors.white,
+    fontSize: 36,
+    fontWeight: FontWeight.w700,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle bold28 = TextStyle(
+    color: Colors.white,
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle bold24 = TextStyle(
+    color: Colors.white,
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle bold48 = TextStyle(
+    color: Colors.white,
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle bold18 = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle normal16 = TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle normal14 = TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.29,
+    letterSpacing: 1,
+  );
+  static const TextStyle semibold14 = TextStyle(
+    color: Color(0xFFF4F4F4),
+    fontSize: 14,
+    fontFamily: 'IBM Plex Sans',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle name = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryText,
+  );
+
+  static const TextStyle role = TextStyle(
+    fontSize: 24,
+    color: AppColors.secondaryText,
+  );
+
+  static const TextStyle navItem = TextStyle(
+    fontSize: 16,
+    color: AppColors.secondaryText,
+  );
+}
