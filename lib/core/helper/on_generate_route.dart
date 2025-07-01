f89cf0cf8d1319b15311434cd2a95ce42865bb1c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 19:41:56 +0300

    Add AboutMeView and update route generation for navigation

--- lib/core/helper/on_generate_route.dart
+++ lib/core/helper/on_generate_route.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/features/about_me/presentation/view/about_me_view.dart';
 import 'package:portfolio/features/home/presentation/view/home_view.dart';
 
 Route<dynamic> onGenerateRoutes(RouteSettings settings) {
   switch (settings.name) {
     case HomeView.routeName:
       return MaterialPageRoute(builder: (context) => const HomeView());
+    case AboutMeView.routeName:
+      return MaterialPageRoute(builder: (context) => const AboutMeView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
---
+++ lib/features/about_me/presentation/view/about_me_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AboutMeView extends StatelessWidget {
+  const AboutMeView({super.key});
+  static const routeName = 'about-me';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
