67dd8223541e383110d4507d0bcf08586fd98f31
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 18:00:54 +0300

    Add UrlLauncher plugin support for Linux and Windows; update pubspec.yaml dependencies

---
+++ lib/features/home/presentation/view/widgets/home_profile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:portfolio/core/utils/assets.dart';
+
+class HomeProfile extends StatelessWidget {
+  const HomeProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 100,
+      backgroundColor: AppColors.accent,
+      child: CircleAvatar(
+        radius: 98,
+        backgroundColor: AppColors.accentBlue,
+        // child: Image.asset(Assets.imagesProfileNoBackground),
+      ),
+    );
+  }
+}
