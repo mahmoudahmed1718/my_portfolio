a06fa519931bd6d3483ffc4508910e8586533cf2
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-01 17:41:38 +0300

    Add main application structure with theme and routing configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/core/helper/on_generate_route.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:portfolio/features/home/presentation/view/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,6 +11,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Mahmoud Ahmed |Software Engineer | Flutter Developer',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColors.background,
+      ),
+      onGenerateRoute: onGenerateRoutes,
+      initialRoute: HomeView.routeName,
+    );
   }
 }
