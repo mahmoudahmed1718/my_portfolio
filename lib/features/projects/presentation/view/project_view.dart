3fe0262aae27ce8df7e93062cc51b15e3fe45252
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-07-02 16:12:33 +0300

    Add ProjectsView to display ProjectsViewBody in the scaffold

--- lib/features/projects/presentation/view/project_view.dart
+++ lib/features/projects/presentation/view/project_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/features/projects/presentation/view/widgets/project_view_body.dart';
 
 class ProjectsView extends StatelessWidget {
   const ProjectsView({super.key});
   static const routeName = 'projects';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: ProjectsViewBody());
   }
 }
